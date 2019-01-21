a3ef75dc2ae34d769478c50abcb6dd2648406a4f
Tevvin <Tevvin7395@icloud.com>
2019-01-21 18:29:17 -0500

    Adding PostContainer & CommentSection

--- InstagramApp/instagram/src/Components/CommentSection/CommentSection.js
+++ InstagramApp/instagram/src/Components/CommentSection/CommentSection.js
@@ -1,8 +1,22 @@
-import react from 'react';
+import React from 'react';
+import './CommentSection.css';
+import instaLike from '../../images/instaLike.png';
+import instaComment from '../../images/instaComment.png';
 
+const CommentSection = props => (
+    <div className="instaComment">
+        <div className="commentId">
+            <img src={instaLike} alt="Like button" />
+            <img src={instaComment} alt="comment button" />
+        </div>
 
-const CommentSection = props => {
-    
-}
+        <h5>{props.likes} like{props.likes !== 1 ? 's' : null}</h5>
+
+        <div className="comments">
+            {props.comments.map(comment => <p className="comment-instance"><span>{comment.username}</span> {comment.text}</p>)}
+        </div>
+        <p>{props.time}</p>
+    </div>
+);
 
 export default CommentSection;
--- InstagramApp/instagram/src/Components/PostContainer/PostContainer.js
+++ InstagramApp/instagram/src/Components/PostContainer/PostContainer.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './PostContainer.css';
-
 import CommentSection from '../CommentSection/CommentSection';
 
-const PostContainer = props => {
-    return <div></div>
-}
+const PostContainer = props => (
+    <div className="content">
+        <div className="contentHeader">
+            <img src={props.dummyData.thumbnailUrl} alt="user thumbnail" />
+            <h4>{props.dummyData.username}</h4>
+        </div>
+
+        <div className='contentImage'>
+            <img src={props.dummyData.imageUrl} alt="User post" />
+            <div></div>
+        </div>
+
+        <CommentSection comments={props.dummyData.comments} likes={props.data.likes} time={props.data.timestamp} />
+    </div>
+);
 
 
 
